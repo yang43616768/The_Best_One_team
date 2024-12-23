3fb1867f6fe2c959c168bf51ce27883cddc0ecf8
YangYang_aaa <2954205878@qq.com>
2024-12-23 20:18:36 +0800

    新建文件夹&初步编译setting文件

---
+++ my-game-project/setting.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+class WindowsSettings:
+    name = 'default game name'
+    width = 1280
+    height = 720
+    OutdoorScale = 1.5
+
+class PlayerSettings:
+    playerSpeed = 5
+    playerWidth = 60
+    playerHeight = 55
+
