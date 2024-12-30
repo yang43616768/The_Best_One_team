ec1ef6a99b0eadd634e6ddc9a6aadb681036fa3d
Pluviophile-wxid <zhaoqa2024@shanghaitech.edu.cn>
2024-12-30 16:27:22 +0800

    Update setting.py
    Co-Authored-By: yang43616768 <106672364+yang43616768@users.noreply.github.com>

--- my-game-project/setting.py
+++ my-game-project/setting.py
@@ -11,3 +11,28 @@ class PlayerSettings:
     playerWidth = 60
     playerHeight = 55
 
+class SceneSettings:
+    tileXnum = 48
+    tileYnum = 27
+    tileWidth = tileHeight = 40
+
+
+class Gamepath:
+    player = r".\assets\player\1.png"
+
+    groundTiles = [
+        r".\assets\tiles\tiles\ground1.png",
+        r".\assets\tiles\tiles\ground2.png",
+        r".\assets\tiles\tiles\ground3.png",
+        r".\assets\tiles\tiles\ground4.png",
+        r".\assets\tiles\tiles\ground5.png",
+        r".\assets\tiles\tiles\ground6.png",
+    ]
+
+    tree = r".\assets\tiles\tiles\tree.png"
+
+class GameState(Enum):
+    MainMenu = 0
+    Game = 1
+    Settings = 2
+    Exit = 3
