b0e199d2d06e6705f40b9357eefd6f6c576f25ee
Pluviophile-wxid <zhaoqa2024@shanghaitech.edu.cn>
2025-01-01 22:22:13 +0800

    我调整了镜头

--- my-game-project/SceneManager.py
+++ my-game-project/SceneManager.py
@@ -5,6 +5,46 @@ from player import *
 from npc import *
 from wall import *
 
+
+# class SceneManager:
+#     def __init__(self, window):
+#         self.map = map.gen_map()
+#         self.window = window
+#         self.clock = pygame.time.Clock()
+#         self.cameraX = 0
+#         self.cameraY = 0
+#         # 调整摄像机的宽度和高度为地图的1/4
+#         self.camera = pygame.Rect(self.cameraX, self.cameraY, WindowsSettings.width / 2, WindowsSettings.height / 2)
+
+#     def tick(self, fps):
+#         self.clock.tick(fps)
+    
+#     def get_width(self):
+#         return WindowsSettings.width * WindowsSettings.OutdoorScale
+    
+#     def get_height(self):
+#         return WindowsSettings.height * WindowsSettings.OutdoorScale
+    
+#     def location(self, obj):
+#         if isinstance(obj, pygame.sprite.Group):
+#             obj.draw(self.window)
+#         elif isinstance(obj, Player) or isinstance(obj, NPC):
+#             obj.draw(self.window)
+#         if isinstance(obj,NPC) and obj.dialogue_active:
+#             obj.draw_dialogue(self.window)
+
+#     def render(self):
+#         for i in range(SceneSettings.tileXnum):
+#             for j in range(SceneSettings.tileYnum):
+#                 self.window.blit(self.map[i][j], (SceneSettings.tileWidth * i, SceneSettings.tileHeight * j))
+
+#     def update_camera(self, player):
+#         self.cameraX = player.rect.x - WindowsSettings.width / 4
+#         self.cameraY = player.rect.y - WindowsSettings.height / 4
+#         # 更新摄像机的宽度和高度为地图的1/4
+#         self.camera = pygame.Rect(self.cameraX, self.cameraY, WindowsSettings.width / 2, WindowsSettings.height / 2)
+#         self.window.blit(self.background, (0, 0), self.camera)
+
 class SceneManager:
     def __init__(self, window):
         self.map = map.gen_map()
@@ -12,7 +52,8 @@ class SceneManager:
         self.clock = pygame.time.Clock()
         self.cameraX = 0
         self.cameraY = 0
-        self.camera = pygame.Rect(self.cameraX, self.cameraY, WindowsSettings.width * WindowsSettings.OutdoorScale, WindowsSettings.height * WindowsSettings.OutdoorScale)
+        # 调整摄像机的宽度和高度为地图的1/4
+        self.camera = pygame.Rect(self.cameraX, self.cameraY, WindowsSettings.width / 2, WindowsSettings.height / 2)
 
     def tick(self, fps):
         self.clock.tick(fps)
@@ -28,16 +69,19 @@ class SceneManager:
             obj.draw(self.window)
         elif isinstance(obj, Player) or isinstance(obj, NPC):
             obj.draw(self.window)
-        if isinstance(obj,NPC) and obj.dialogue_active:
+        if isinstance(obj, NPC) and obj.dialogue_active:
             obj.draw_dialogue(self.window)
 
     def render(self):
         for i in range(SceneSettings.tileXnum):
             for j in range(SceneSettings.tileYnum):
-                self.window.blit(self.map[i][j], (SceneSettings.tileWidth * i, SceneSettings.tileHeight * j))
+                tile = self.map[i][j]
+                tile_rect = tile.get_rect(topleft=(SceneSettings.tileWidth * i, SceneSettings.tileHeight * j))
+                if self.camera.colliderect(tile_rect):
+                    self.window.blit(tile, (tile_rect.x - self.camera.x, tile_rect.y - self.camera.y))
 
-    def update_camera(self,player):
-        self.cameraX = player.rect.x - WindowsSettings.width / 2
-        self.cameraY = player.rect.y - WindowsSettings.height / 2
-        self.camera = pygame.Rect(self.cameraX, self.cameraY, WindowsSettings.width * WindowsSettings.OutdoorScale, WindowsSettings.height * WindowsSettings.OutdoorScale)
-        self.window.blit(self.background, (0, 0), self.camera)
+    def update_camera(self, player):
+        self.cameraX = player.rect.x - self.camera.width / 2
+        self.cameraY = player.rect.y - self.camera.height / 2
+        # 更新摄像机的宽度和高度为地图的1/4
+        self.camera = pygame.Rect(self.cameraX, self.cameraY, WindowsSettings.width / 2, WindowsSettings.height / 2)
