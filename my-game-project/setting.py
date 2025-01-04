d349fc9b20d8a7550f53df31a053470a2e875892
YangYang_aaa <2954205878@qq.com>
2025-01-04 19:46:05 +0800

    bug fixed

--- my-game-project/setting.py
+++ my-game-project/setting.py
@@ -29,6 +29,7 @@ class NpcSettings:
     5.The player can move with the "WASD" keys.
     6.The player can fight with the NPC by pressing the "F" key.
     7.The player can purchase resources from the NPCs by pressing the "P" key.'''
+    dialogue1 = {"role": "system", "content":Task_Alice}
     items1 = [("Item 1", 10), ("Item 2", 20), ("Item 3", 30)]
     photopath1_1 = r".\assets\images\npc1.1.png"
     photopath1_2 = r".\assets\images\npc1.2.png"
@@ -36,7 +37,7 @@ class NpcSettings:
     currency1 = 100
     health1 = 50
     attack1 = 20
-    Alice = [Task_Alice,items1,photopath1_1,photopath1_2,reward1,currency1,health1,attack1]
+    Alice = [dialogue1,items1,photopath1_1,photopath1_2,reward1,currency1,health1,attack1]
 class SceneSettings:
     tileXnum = 60
     tileYnum = 36
